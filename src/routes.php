a41646ed31037fc3ea3efd2aa07f70cb773fba27
Pablo Andrés Anello <pabloanello@gmail.com>
2017-01-03 02:07:48 -0300

    first route config

--- src/routes.php
+++ src/routes.php
@@ -8,3 +8,9 @@ $app->get('/[{name}]', function ($request, $response, $args) {
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->get('/profile/facebook/{id}', function ($request, $response, $args) {
+    $response->getBody()->write("Hello, Facebook User: ".$args['id']);
+
+    return $response;
+});
