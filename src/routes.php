83fac4720362a12ab38d8c7d157989e88499a020
Pablo Andrés Anello <pabloanello@gmail.com>
2017-01-04 01:42:01 -0300

    get facebook user data

--- src/routes.php
+++ src/routes.php
@@ -1,16 +1,25 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
-});
-
 $app->get('/profile/facebook/{id}', function ($request, $response, $args) {
-    $response->getBody()->write("Hello, Facebook User: ".$args['id']);
+    
+    $fb = new Facebook\Facebook([
+  		'app_id' => '643931279111443',
+  		'app_secret' => '4b27dbed273f80adbcc5072f0688cb18',
+  		'default_graph_version' => 'v2.2',
+  	]);
 
-    return $response;
+	try {
+  		$fbData = $fb->get('/'.$args['id'].'?fields=first_name,last_name','EAAJJpu8mtRMBAPkAxkeGqVCcFNznQa54ffsKYcBRfU2BWqPuLVS3sNimpNCWtDeDcLVFoHToDyRAVMT7CbbZCXR2ceMNTO50e49cvgzmsndry7ZB34yqxaEbsxKeuZCmpEjPv1HZBTGfICfTOkwo7sZA6fSqfzSH4Ggbw4B2CrD4821pQg3ZCQVmI6HMutBl0ZD');
+  		return $response->withStatus(200)
+        ->withHeader('Content-Type', 'application/json')
+        ->write($fbData->getBody());
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  		echo 'Graph returned an error: ' . $e->getMessage();
+  		exit;
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  		exit;
+	}
+
 });
